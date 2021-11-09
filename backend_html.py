7a70c5e3e891b1a78669d341fe4f4003e2b0bfa5
quah ming jun <MJQUAH20.20@ichat.sp.edu.sg>
2021-11-09 20:25:33 +0800

    CHANGED

--- backend_html.py
+++ backend_html.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 
 @app.route("/", methods=('GET', 'POST'))
 def index():
+    # if request.method=='POST':
+        # if request.form[""]
     return render_template('index.html')
 
 if __name__ == '__main__':
